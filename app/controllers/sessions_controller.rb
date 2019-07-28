6989f2be350a77abeccab0fc020ad3935d29e9d3
cyantis <cyantis@gmail.com>
2019-07-27 18:03:55 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def new
+    @users = User.all
+  end
+
+  def create
+    @user = User.find_by(name: params[:user_name])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  root 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
 end
