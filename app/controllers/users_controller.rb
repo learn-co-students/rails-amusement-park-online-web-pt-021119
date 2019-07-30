e74d60a2e62b4d5c6d6a7143ad24cee2bd8f381b
cyantis <cyantis@gmail.com>
2019-07-29 18:42:57 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,27 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: session[:user_id])
+    if logged_in?
+      @user = current_user
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @attraction = Attraction.find_by(id: params[:user][:attraction_id])
+    @user = current_user
+    @ride = Ride.new
+    @ride.user = @user
+    @ride.attraction = @attraction
+    @ride.save
+    flash[:alert] = @ride.take_ride
+    redirect_to user_path(current_user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets)
+    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  root 'users#new'
+  root 'sessions#new'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  resources :admins, only: [:show]
+  resources :attractions
 end
