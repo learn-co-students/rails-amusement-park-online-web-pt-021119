13e57fa0b4d96610f99e32be82bc7fe539fd8bbc
chilbert <chase@chasehilbert.com>
2019-08-01 16:32:12 -0500

    Refactor of User and Ride methods

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,18 +6,18 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    if self.user.tickets >= self.attraction.tickets && self.user.height >= self.attraction.min_height
-      @user = User.find(self.user_id)
+    if user.tickets >= attraction.tickets && user.height >= attraction.min_height
+      @user = User.find(user_id)
       @user.tickets -= attraction.tickets
       @user.nausea += attraction.nausea_rating
       @user.happiness += attraction.happiness_rating
       @user.save
-    elsif self.user.height >= self.attraction.min_height && self.user.tickets < self.attraction.tickets
+    elsif user.height >= attraction.min_height && user.tickets < attraction.tickets
       return "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    elsif self.user.height <= self.attraction.min_height && self.user.tickets >= self.attraction.tickets
+    elsif user.height <= attraction.min_height && user.tickets >= attraction.tickets
       return "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
-    return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-end
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    end
   end
 end
