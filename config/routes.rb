d6ee4dae49168861cbb5c90c8aae97a1b929aaa3
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-08-15 23:37:31 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#home'
+
+  resources :users
+  resources :admins, only: [:show]
+  resources :attractions
+  resources :rides
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+  post '/take_ride' => 'attractions#take_ride'
+
+
 end
